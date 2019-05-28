e063b27a15613de273ef0dda0a1efce1744cdb65
Cuiqing_Li <lixx3527@gmail.com>
2019-05-28 13:59:07 +0800

    training part has been finsihed, now we need to optimize codes

--- data_generator.py
+++ data_generator.py
@@ -4,6 +4,12 @@ import skimage.io as io
 import skimage.transform as transform
 from random import randint
 from random import shuffle
+import os
+import cv2
+import scipy.misc
+import numpy as np 
+import math
+import json
 
 #images loaded in "paired" setting
 def load_images_paired(img_names,is_train=True, true_size = 256, enlarge_size = 286):
@@ -29,6 +35,236 @@ def load_images_paired(img_names,is_train=True, true_size = 256, enlarge_size = 
     iter += 1  
   
   return A_imgs,B_imgs
+
+def read_frame(file_path, crop=False, ih=0, iw=0, resize=False, rh=0, rw=0, norm=True, bias=1, crop_h_flag=0, args=None):
+    f = cv2.imread(file_path)
+    f = np.array(f)
+    f = f.astype(np.float32)
+    if norm:
+        f = f / 127.5 - bias
+    if crop:
+        if not crop_h_flag:
+            down_px = 0 # gimp: 380-1100
+            f = f[400-down_px :1120- down_px , 0:720] # lower crop 1130 
+            # center crop 654 x 654 from 720 x 720 -- scale 1.1
+            scale = 1.0 
+            if scale > 1:
+                st = int((720 - 720/scale)/2)
+                ed = st+int(720/scale)
+                f = f[st:ed, st:ed]
+
+
+            elif scale > 0 and scale < 1:
+                side_len = int(720/scale)
+                f_cache = np.zeros((side_len, side_len, 3), dtype=float)
+                # pos = [0, 0]
+                f_cache -= 1 # color black -> value -1
+                st = int((720/scale - 720)/2)
+                ed = st+720
+                f_cache[st:ed, st:ed] = f
+                f = f_cache
+
+
+        elif crop_h_flag in [10, 20, 21, 30]:
+
+            # f = f[args.crop_pos_x:(args.crop_pos_x+args.crop_scale_h), args.crop_pos_y:(args.crop_pos_y+args.crop_scale_w)]
+            f_cache = np.zeros((args.crop_scale_h, args.crop_scale_w, 3), dtype=float)
+            f_cache -= 1 # color black -> value -1
+
+            x_l = max([args.crop_pos_x, 0])
+            x_r = min([args.crop_pos_x+args.crop_scale_h, f.shape[0]]) # h # scipy.misc.imread -> h x w x c
+            y_l = max([args.crop_pos_y, 0])
+            y_r = min([args.crop_pos_y+args.crop_scale_w, f.shape[1]]) # w
+
+            f_cache[(x_l-args.crop_pos_x):(x_r-args.crop_pos_x), (y_l-args.crop_pos_y):(y_r-args.crop_pos_y)] = f[x_l:x_r, y_l:y_r]
+            f = f_cache
+
+        else:
+            ih = f.shape[0]
+            iw = f.shape[1]
+            crop_size = min([ih, iw])
+            f = f[int(ih/2 - crop_size/2) : int(ih/2 + crop_size/2), int(iw/2 - crop_size/2): int(iw/2 + crop_size/2)]
+
+    if args.scale_aug:
+        scale = args.scale
+
+        f_cache = np.zeros(f.shape, dtype=float)
+        f_cache -= 1
+        if scale >= 1:
+            f_cache[int(f.shape[0]*(1-1/scale)/2):int(f.shape[0]*((1-1/scale)/2))+int(f.shape[0]*1/scale), int(f.shape[1]*(1-1/scale)/2):int(f.shape[1]*((1-1/scale)/2))+int(f.shape[1]*1/scale)] = cv2.resize(f, (int(f.shape[0]/scale), int(f.shape[1]/scale)), interpolation=cv2.INTER_CUBIC)
+        else:
+            f_cache = cv2.resize(f, (int(f.shape[0]/scale), int(f.shape[1]/scale)), interpolation=cv2.INTER_CUBIC)[int(f.shape[0]*(1/scale-1)/2):int(f.shape[0]*((1/scale-1)/2+1)), int(f.shape[1]*(1/scale-1)/2):int(f.shape[1]*((1/scale-1)/2+1) )]
+
+        f = f_cache
+
+    if resize: 
+        f = cv2.resize(f, (rw, rh), interpolation=cv2.INTER_CUBIC)
+
+    return f
+
+def read_image_and_resize(args, folder_index, image_index, target_image_index):
+
+
+        IMAGE_WIDTH = args.resize_w 
+        IMAGE_HEIGHT = args.resize_h
+
+        out_x = np.zeros( (IMAGE_HEIGHT, IMAGE_WIDTH, 3*3*args.frame_count)) # channel concatenate.
+
+        # image_index = pair_element[0]
+        # folder_index = pair_element[1]
+        # target_image_index = pair_element[2]
+
+        sample_folder_full = os.path.join(args.dataroot, str(folder_index))
+        crop_h_flag = args.crop_h_flag
+
+        if crop_h_flag == 20:
+            # read json f1
+            i_file_j = sample_folder_full+'/json/j_1.json'
+
+            fp = open(i_file_j, 'r')
+            f_content = fp.read()
+            fp.close()
+            j = json.loads(f_content)
+
+            hyp_para_crop_w = 1.98
+            hyp_para_crop_h = 2.53
+            hyp_para_crop_x = 0.5 # in width direction
+            hyp_para_crop_y = 1.07
+
+            crop_scale_w = int((j['points'][16][0] - j['points'][0][0]) * hyp_para_crop_w)
+            crop_scale_h = int((j['points'][8][1] - 0.5 * j['points'][16][1] - 0.5 * j['points'][0][1] ) * hyp_para_crop_h)
+            crop_pos_y = int(j['points'][0][0] - hyp_para_crop_x * (j['points'][16][0] - j['points'][0][0]))
+            crop_pos_x = int(0.5*(j['points'][16][1] + j['points'][0][1]) - hyp_para_crop_y * (j['points'][8][1] - 0.5* (j['points'][16][1] + j['points'][0][1]) ) )
+
+        if crop_h_flag == 30: # h=w in crop_h_flag 20
+            # read json f1
+            i_file_j = sample_folder_full+'/json/j_1.json'
+
+            fp = open(i_file_j, 'r')
+            f_content = fp.read()
+            fp.close()
+            j = json.loads(f_content)
+
+            hyp_para_crop_w = 1.98
+            hyp_para_crop_h = 2.53
+            hyp_para_crop_x = 0.5 # in width direction
+            hyp_para_crop_y = 1.07
+
+            crop_scale_w = int((j['points'][16][0] - j['points'][0][0]) * hyp_para_crop_w)
+            crop_scale_h = int((j['points'][16][0] - j['points'][0][0]) * hyp_para_crop_w)
+            crop_pos_y = int(j['points'][0][0] - hyp_para_crop_x * (j['points'][16][0] - j['points'][0][0]))
+            crop_pos_x = int(0.5*(j['points'][16][1] + j['points'][0][1]) - hyp_para_crop_y * (j['points'][8][1] - 0.5* (j['points'][16][1] + j['points'][0][1]) ) )
+
+        if crop_h_flag == 21: # use points 39 44 55
+            # read json f1
+            i_file_j = sample_folder_full+'/json/j_1.json'
+
+            fp = open(i_file_j, 'r')
+            f_content = fp.read()
+            fp.close()
+            j = json.loads(f_content)
+
+            points=[j['points'][38], j['points'][43], j['points'][54] ]
+
+            hyp_para_crop_w = 7.815
+            hyp_para_crop_h = 8.576
+            hyp_para_crop_x = 3.364 # in width direction
+            hyp_para_crop_y = 3.822
+
+            crop_scale_w = int((points[1][0] - points[0][0]) * hyp_para_crop_w)
+            # self.args.crop_scale_h = int((points[2][1] - 0.5 * points[1][1] - 0.5 * points[0][1] ) * hyp_para_crop_h)
+            crop_scale_h = crop_scale_w
+
+            crop_pos_y = int(points[0][0] - hyp_para_crop_x * (points[1][0] - points[0][0]))
+            crop_pos_x = int(0.5*(points[1][1] + points[0][1]) - hyp_para_crop_y * (points[2][1] - 0.5* (points[1][1] + points[0][1]) ) )
+
+
+        for i in range(args.frame_count):
+            image_extract_id = image_index + i
+
+            PNCC_folder_path = os.path.join(sample_folder_full, 'PNCC')
+            extract_image_path = PNCC_folder_path + "/p_" + str(image_extract_id) + ".png"
+            out_x[ :, :, i*9:i*9+3] =  read_frame(extract_image_path, crop=not args.no_crop, ih=args.img_height, iw=args.img_width, resize=True, \
+                rh=IMAGE_HEIGHT, rw=IMAGE_WIDTH, bias=1, crop_h_flag=crop_h_flag, args=args)
+            
+
+
+            dtex_folder_path = os.path.join(sample_folder_full, '3dTex')
+            extract_image_path = dtex_folder_path + "/t_" + str(image_extract_id) + ".png"
+            out_x[ :, :, i*9+3:i*9+6] =  read_frame(extract_image_path, crop=not args.no_crop, ih=args.img_height, iw=args.img_width, resize=True, \
+                rh=IMAGE_HEIGHT, rw=IMAGE_WIDTH, bias=1, crop_h_flag=crop_h_flag, args=args)
+
+            densepose_folder_path = os.path.join(sample_folder_full, 'densepose')
+            extract_image_path = densepose_folder_path + "/f_" + str(image_extract_id) + "_IUV.png"
+            out_x[ :, :, i*9+6:i*9+9] =  read_frame(extract_image_path, crop=not args.no_crop, ih=args.img_height, iw=args.img_width, resize=True, \
+                rh=IMAGE_HEIGHT, rw=IMAGE_WIDTH, bias=1, crop_h_flag=crop_h_flag, args=args)
+
+
+        target_img = ""
+        if args.mode == 'train':
+            face_folder_path = os.path.join(sample_folder_full, 'face')
+            extract_image_path = face_folder_path + "/f_" + str(target_image_index) + ".png"
+
+            out_y = read_frame(extract_image_path, crop=not args.no_crop, ih=args.img_height, iw=args.img_width, resize=True, \
+                rh=IMAGE_HEIGHT, rw=IMAGE_WIDTH, bias=1, crop_h_flag=crop_h_flag, args=args)
+
+            # out_x = tf.convert_to_tensor(out_x, np.float32)
+            # out_y = tf.convert_to_tensor(out_y, np.float32)
+            #out_x = out_x.eval()
+            #out_y = out_y.eval()
+            return (out_x, out_y)
+        else:
+
+            # out_x = tf.convert_to_tensor(out_x, np.float32)
+
+            #out_x = out_x.eval()
+            return (out_x, )
+
+def load_images_paired2(args, cur_index, batch_size, image_size, frame_count, train_list):
+  A_imgs = np.zeros((batch_size,image_size,image_size,9*args.frame_count)) # ASSUMING RGB FOR NOW
+  B_imgs= np.zeros((batch_size, image_size, image_size, 3))
+
+
+  for batch_id in range(batch_size):
+    image_index = train_list[cur_index+batch_id][1]
+    folder_index = train_list[cur_index+batch_id][0]
+    target_image_index = train_list[cur_index+batch_id][2]
+
+    out_x, out_y = read_image_and_resize(args, folder_index, image_index, target_image_index)
+    A_imgs[batch_id,:,:,:] = out_x
+    B_imgs[batch_id,:,:,:] = out_y
+
+
+  return A_imgs, B_imgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def flip_image(img,flipflag):
   if flipflag:
--- pix2pixHD_network.py
+++ pix2pixHD_network.py
@@ -16,76 +16,80 @@ class pix2pixHD_network(object):
 
 	def generator_output(self,image_A_input):
 		#G2 structure:
-		scope_name = 'G2_conv1'
+		scope_name = 'gen_G2_conv1'
 		self.G2_conv1 = conv(image_A_input, filter_height=3, filter_width=3, num_outputs=self.args.ngf, stride_y=1, stride_x=1, name = scope_name)
 		self.G2_conv1_batchnorm = apply_batchnorm(self.G2_conv1, name=scope_name)
 		self.G2_conv1_activation = lrelu(self.G2_conv1_batchnorm, lrelu_alpha=0.2, name=scope_name)
-		scope_name = 'G2_conv1_downsampling'
+		scope_name = 'gen_G2_conv1_downsampling'
 		self.G2_conv1_downsampling = conv(self.G2_conv1_activation, filter_height=3, filter_width=3, num_outputs=self.args.ngf*2, stride_y=2, stride_x=2, name = scope_name)
 		self.G2_conv1_downsampling_bnorm = apply_batchnorm(self.G2_conv1_downsampling, name=scope_name)
 		self.G2_conv1_dwonsampling_activation = lrelu(self.G2_conv1_downsampling_bnorm, lrelu_alpha=0.2, name=scope_name)
 
 		# G1 structure  downsampling
-		scope_name = 'G1_pre_block':
+		scope_name = 'gen_G1_pre_block'
 		self.G1_pre_block = conv(self.G2_conv1_dwonsampling_activation, filter_height=3, filter_width=3, num_outputs=self.args.ngf*2, stride_y=1,stride_x=1, name=scope_name)
 		self.G1_pre_block_activation = lrelu(apply_batchnorm(self.G1_pre_block, name = scope_name), lrelu_alpha=0.2, name=scope_name)
 
-		scope_name = 'G1_downsampling1'
+		scope_name = 'gen_G1_downsampling1'
 		self.G1_downsampling1 = conv(self.G1_pre_block_activation, filter_height=3, filter_width=3, num_outputs=self.args.ngf*4, stride_y=2, stride_x=2, name=scope_name)
-		scope_name = 'G1_downsampling2'
+		scope_name = 'gen_G1_downsampling2'
 		self.G1_downsampling2 = conv(self.G1_downsampling1, filter_height=3, filter_width=3, num_outputs=self.args.ngf*8, stride_y=2, stride_x=2, name=scope_name)
-		scope_name = 'G1_downsampling3'
+		scope_name = 'gen_G1_downsampling3'
 		self.G1_downsampling3 = conv(self.G1_downsampling2, filter_height=3, filter_width=3, num_outputs=self.args.ngf*16, stride_y=2, stride_x=2, name=scope_name)
 
 		# G1 residual_blocks
-		scope_name = 'G1_res_block1'
-		self.G1_res_block1 = conv(self.G1_downsampling2, filter_height=3, filter_width=3, num_outputs=self.args.ngf*16, stride_y=1, stride_x=1, name=scope_name)
+		scope_name = 'gen_G1_res_block1'
+		self.G1_res_block1 = conv(self.G1_downsampling3, filter_height=3, filter_width=3, num_outputs=self.args.ngf*16, stride_y=1, stride_x=1, name=scope_name)
 		self.G1_res_block1_activation = lrelu(apply_batchnorm(self.G1_res_block1, name=scope_name), lrelu_alpha=0.2, name=scope_name)
-		scope_name = 'G1_res_block2'
+		scope_name = 'gen_G1_res_block2'
 		self.G1_res_block2 = conv(self.G1_res_block1_activation, filter_height=3, filter_width=3, num_outputs=self.args.ngf*16, stride_y=1, stride_x=1, name=scope_name)
 		self.G1_res_block2_activation = lrelu(apply_batchnorm(self.G1_res_block2, name=scope_name), lrelu_alpha=0.2, name=scope_name)
-		scope_name = 'G1_res_block3'
+		scope_name = 'gen_G1_res_block3'
 		self.G1_res_block3 = conv(self.G1_res_block2_activation, filter_height=3, filter_width=3, num_outputs=self.args.ngf*16, stride_y=1, stride_x=1, name=scope_name)
 		self.G1_res_block3_activation = lrelu(apply_batchnorm(self.G1_res_block3, name=scope_name), lrelu_alpha=0.2, name=scope_name)
-		scope_name = 'G1_res_block4'
+		scope_name = 'gen_G1_res_block4'
 		self.G1_res_block4 = conv(self.G1_res_block3_activation, filter_height=3, filter_width=3, num_outputs=self.args.ngf*16, stride_y=1, stride_x=1, name=scope_name)
 		self.G1_res_block4_activation = lrelu(apply_batchnorm(self.G1_res_block4, name=scope_name), lrelu_alpha=0.2, name=scope_name)
-		scope_name = 'G1_res_block5'
+		scope_name = 'gen_G1_res_block5'
 		self.G1_res_block5 = conv(self.G1_res_block4_activation, filter_height=3, filter_width=3, num_outputs=self.args.ngf*16, stride_y=1, stride_x=1, name=scope_name)
 		self.G1_res_block5_activation = lrelu(apply_batchnorm(self.G1_res_block5, name=scope_name), lrelu_alpha=0.2, name=scope_name)
-		scope_name = 'G1_res_block6'
+		scope_name = 'gen_G1_res_block6'
 		self.G1_res_block6 = conv(self.G1_res_block5_activation, filter_height=3, filter_width=3, num_outputs=self.args.ngf*16, stride_y=1, stride_x=1, name=scope_name)
 		self.G1_res_block6_activation = lrelu(apply_batchnorm(self.G1_res_block6, name=scope_name), lrelu_alpha=0.2, name=scope_name)
 
 
 		#G1_upsampling
 
-		scope_name = "G1_upsampling1"
-		self.G1_upsampling1 = deconv(self.G1_res_block6_activation, filter_height=3, filter_width=3, num_outputs=args.ngf*8, batch_size=self.args.batch_size, stride_y=2, stride_x=2, name=scope_name, padding='SAME')
-		scope_name = "G1_upsampling2"
-		self.G1_upsampling2 = deconv(self.G1_upsampling1, filter_height=3, filter_width=3, num_outputs=args.ngf*4, batch_size=self.args.batch_size, stride_y=2, stride_x=2, name=scope_name, padding='SAME')
-		scope_name = "G1_downsampling3"
-		self.G1_upsampling3 = deconv(self.G1_upsampling2, filter_height=3, filter_width=3, num_outputs=args.ngf*2, batch_size=self.args.batch_size, stride_y=2, stride_x=2, name=scope_name, padding='SAME')
+		scope_name = "gen_G1_upsampling1"
+		self.G1_upsampling1 = deconv(self.G1_res_block6_activation, filter_height=3, filter_width=3, num_outputs=self.args.ngf*8, \
+			batch_size=self.args.batch_size, stride_y=2, stride_x=2, name=scope_name, padding='SAME')
+		scope_name = "gen_G1_upsampling2"
+		self.G1_upsampling2 = deconv(self.G1_upsampling1, filter_height=3, filter_width=3, num_outputs=self.args.ngf*4, \
+			batch_size=self.args.batch_size, stride_y=2, stride_x=2, name=scope_name, padding='SAME')
+		scope_name = "gen_G1_upsampling3"
+		self.G1_upsampling3 = deconv(self.G1_upsampling2, filter_height=3, filter_width=3, num_outputs=self.args.ngf*2, \
+			batch_size=self.args.batch_size, stride_y=2, stride_x=2, name=scope_name, padding='SAME')
 		self.G1_upsampling3_activation = lrelu(apply_batchnorm(self.G1_upsampling3, name=scope_name), lrelu_alpha=0.2, name=scope_name)
 		
 		# G2 structure
 		self.G2_conv2 = tf.add(self.G1_upsampling3_activation, self.G2_conv1_dwonsampling_activation)
 
 		# G2 res_block area
-		scope_name = 'G2_res_block1'
+		scope_name = 'gen_G2_res_block1'
 		self.G2_res_block1 = conv(self.G2_conv2, filter_height=3, filter_width=3, num_outputs=self.args.ngf*2, stride_y=1, stride_x=1, name=scope_name)
 		self.G2_res_block1_activation = lrelu(apply_batchnorm(self.G2_res_block1, name=scope_name), lrelu_alpha=0.2, name=scope_name)
-		scope_name = 'G2_res_block2'
+		scope_name = 'gen_G2_res_block2'
 		self.G2_res_block2 = conv(self.G2_res_block1_activation, filter_height=3, filter_width=3, num_outputs=self.args.ngf*2, stride_y=1, stride_x=1, name=scope_name)
 		self.G2_res_block2_activation = lrelu(apply_batchnorm(self.G2_res_block2, name=scope_name), lrelu_alpha=0.2, name=scope_name)
-		scope_name = "G2_res_block3"
+		scope_name = "gen_G2_res_block3"
 		self.G2_res_block3 = conv(self.G2_res_block2_activation, filter_height=3, filter_width=3, num_outputs=self.args.ngf*2, stride_y=1, stride_x=1, name=scope_name)
 		self.G2_res_block3_activation = lrelu(apply_batchnorm(self.G2_res_block3, name=scope_name), lrelu_alpha=0.2, name=scope_name)
 
 
-		scope_name = "G2_upsampling"
-		self.G2_upsampling = deconv(self.G2_res_block3_activation, filter_height=3, filter_width=3, num_outputs=args.ngf, batch_size=self.args.batch_size, stride_y=2, stride_x=2, name=scope_name, padding='SAME')
-		scope_name = 'generator_output_layer'
+		scope_name = "gen_G2_upsampling"
+		self.G2_upsampling = deconv(self.G2_res_block3_activation, filter_height=3, filter_width=3, num_outputs=self.args.ngf, batch_size=self.args.batch_size, \
+			stride_y=2, stride_x=2, name=scope_name, padding='SAME')
+		scope_name = 'gen_generator_output_layer'
 		self.generator_output_layer = conv(self.G2_upsampling, filter_height=3, filter_width=3, num_outputs=3, stride_y=1, stride_x=1, name = scope_name)
 		self.generator_output_layer_activation = lrelu(apply_batchnorm(self.generator_output_layer, name=scope_name), lrelu_alpha=0.2, name=scope_name)
 
@@ -95,10 +99,44 @@ class pix2pixHD_network(object):
 		return self.generator_output_layer_activation
 
 	def discriminator_output(self, B_input):
-		pass
+		discrim_input = tf.concat([self.image_A,B_input],3)
+		scope_name = 'dis_conv1'
+		self.dis_conv1 = lrelu(conv(discrim_input,4,4,64,2,2,padding='SAME',name=scope_name),lrelu_alpha=0.2, name=scope_name)
+		scope_name = 'dis_conv2'
+		self.dis_conv2 = lrelu(apply_batchnorm(conv(self.dis_conv1,4,4,128,2,2,padding='SAME',name=scope_name),name=scope_name),lrelu_alpha=0.2, name=scope_name)
+		scope_name = 'dis_conv3'
+		self.dis_conv3 = lrelu(apply_batchnorm(conv(self.dis_conv2,4,4,256,2,2,padding='SAME',name=scope_name),name=scope_name),lrelu_alpha=0.2, name=scope_name)
+		scope_name = 'dis_conv4'
+		self.dis_conv4 = lrelu(apply_batchnorm(conv(self.dis_conv3,4,4,512,1,1,padding='SAME',name=scope_name),name=scope_name),lrelu_alpha=0.2, name=scope_name)
+		scope_name = 'dis_conv5'
+		self.dis_conv5 = conv(self.dis_conv4,4,4,1,1,1,padding='SAME',name=scope_name)
+		self.dis_out_per_patch = tf.reshape(self.dis_conv5,[self.batch_size,-1])
+		return tf.sigmoid(self.dis_out_per_patch)
 
 	def compute_loss(self):
-		pass
+		eps = 1e-12
+		fake_B = self.generator_output(self.image_A)
+		fake_output_D = self.discriminator_output(fake_B)
+		real_output_D = self.discriminator_output(self.image_B)		
+		self.d_loss = tf.reduce_mean(-(tf.log(real_output_D + eps) + tf.log(1 - fake_output_D + eps)))     
+		self.g_loss_l1= self.l1_Weight*tf.reduce_mean(tf.abs(fake_B - self.image_B))
+		self.g_loss_gan = tf.reduce_mean(-tf.log(fake_output_D + eps))
+		return self.d_loss, self.g_loss_l1 + self.g_loss_gan, self.g_loss_l1, self.g_loss_gan
 
 	def load_initial_weights(self, session):
-		pass
+		# Load the weights into memory: this approach is adopted rather than standard random initialization to allow the
+		# flexibility to load weights from a numpy file or other files.
+		if self.WEIGHTS_PATH:
+			print('loading initial weights from '+ self.WEIGHTS_PATH)
+			weights_dict = np.load(self.WEIGHTS_PATH, encoding = 'bytes').item()    
+		# else:
+		# 	print 'loading random weights'
+		# 	weights_dict = get_random_weight_dictionary('pix2pix_initial_weights')
+		# Loop over all layer names stored in the weights dict
+		for op_name in weights_dict:          
+			print(op_name)
+			with tf.variable_scope(op_name) as scope:  
+				for sub_op_name in weights_dict[op_name]:
+	  				data = weights_dict[op_name][sub_op_name]
+	  				var = get_scope_variable(name, sub_op_name, shape=[data.shape[0], data.shape[1], data.shape[2], data.shape[3]])
+	  				session.run(var.assign(data))
